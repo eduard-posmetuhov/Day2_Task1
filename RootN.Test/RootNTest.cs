0ad5a2893af2107efc28bbcfaf6addf9fed411d0
eduard-posmetuhov <eduard.posmetuhov@gmail.com>
2015-07-13 13:36:02 +0200

    Task1

---
+++ RootN.Test/RootNTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RootN;
+
+namespace RootN.Test
+{
+    [TestClass]
+    public class RootNTest
+    {
+        [TestMethod]
+        public void Positive1()
+        {
+            Assert.AreEqual((int)Root.FindRoot(999.0, 3, 7) * 100, (int)Math.Pow(999.0, 1.0 / 3)*100);
+        }
+
+        [TestMethod]
+        public void Positive2()
+        {
+            Assert.AreEqual((int)Root.FindRoot(999.0, 2, 7) * 100, (int)Math.Pow(999.0, 1.0 / 2) * 100);
+        }
+
+        [TestMethod]
+        public void Positive3()
+        {
+            Assert.AreEqual((int)Root.FindRoot(-999.0, 3, 7) * 100, (int)Math.Pow(999.0, 1.0 / 3) * -100);
+        }
+
+        [TestMethod]
+        public void Zero()
+        {
+            Assert.AreEqual((int)Root.FindRoot(0, 3, 7) * 100, (int)Math.Pow(0, 1.0 / 3) * 100);
+        }
+
+        [TestMethod]
+        public void nLessZero()
+        {
+            try
+            {
+                Root.FindRoot(999, -3, 7);
+            }
+            catch (ArgumentException e)
+            {
+                StringAssert.Contains(e.Message, "Степень должна быть положительная");
+            }
+        }
+
+        [TestMethod]
+        public void NoDecision()
+        {
+            try
+            {
+                Root.FindRoot(-225, 2, 7);
+            }
+            catch (InvalidOperationException e)
+            {
+                StringAssert.Contains(e.Message, "Нет решения");
+            }
+        }
+    }
+}
---
+++ RootN/Root.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RootN
+{
+    public class Root
+    {
+        public static double FindRoot(double N, int n, int e)
+        {
+            if (n % 2 == 0 && N < 0)
+                throw new InvalidOperationException("Нет решения");
+            if (n<0)
+                throw new ArgumentException("Степень должна быть положительная");
+            double x=2,nx=1;
+            double eps = Math.Pow(10,-e);
+            while (Math.Abs(x - nx) > eps)
+            {
+                 x = nx;
+                 nx = ((n-1)*nx+N/Math.Pow(nx,n-1))/n;
+                 
+            }
+            return x;            
+        }
+    }
+}
